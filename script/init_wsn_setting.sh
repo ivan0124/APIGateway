6dc441f4d3cb51b1a0f55e34b2676d9ee6156e13
ivan0124 <ivan0124.tw@gmail.com>
2016-12-05 16:15:53 +0800

    [APIGateway] copy wsn_setting files to host shared folder Date            : 2016/12/05 Bug #           : N/A Description     : copy wsn_setting files to host shared folder Files           : Reviewer        : Commit by       : Ivan.Chen

--- script/init_wsn_setting.sh
+++ script/init_wsn_setting.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+#copy wsn_setting files
+cp -rf /home/adv/api_gw/apps/wsn_manage/wsn_setting/* /home/adv/wsn_setting/
+
 #only remove subdirectoy
 mkdir -p /home/adv/wsn_setting/device_html
 cd /home/adv/wsn_setting/device_html
