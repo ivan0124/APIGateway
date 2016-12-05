8c6937c8d84e24ed22fd338db590b5eafee6e6f7
ivan <ivan0124.tw@gmail.com>
2016-12-05 15:16:07 +0800

    [APIGateway] update init_wsn_setting.sh
    [APIGateway] update init_wsn_setting.sh
    Date            : 2016/12/05
    Bug #           : N/A
    Description     : update init_wsn_setting.sh
    Files           :
    Reviewer        :
    Commit by       : Ivan.Chen

--- script/init_wsn_setting.sh
+++ script/init_wsn_setting.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
 #only remove subdirectoy
-mkdir /home/adv/wsn_setting/device_html
+mkdir -p /home/adv/wsn_setting/device_html
 cd /home/adv/wsn_setting/device_html
 rm -Rf -- */
 
 #remove all files
-mkdir /home/adv/wsn_setting/device_table
+mkdir -p /home/adv/wsn_setting/device_table
 cd /home/adv/wsn_setting/device_table
 rm -rf *
 
